c15b2d7b1a539847ee9b7164e74f0c5780c4aac9
Sasha Jenner <jenner.sasha@gmail.com>
2019-12-15 15:58:41 +1100

    updated

--- run_realf5p.sh
+++ run_realf5p.sh
@@ -1,4 +1,5 @@
-bash monitor/monitor.sh testing/simulator_out/fast5/ testing/simulator_out/fastq/ >
-usr/bin/time -v ./f5pl_realtime data/ip_list.cfg
+#bash monitor/monitor.sh testing/simulator_out/fast5/ testing/simulator_out/fastq/ > /usr/bin/time -v ./f5pl_realtime data/ip_list.cfg # no permission to /usr/bin/time ;(
+
+bash monitor/monitor.sh testing/simulator_out/fast5/ testing/simulator_out/fastq/ > ./f5pl_realtime data/ip_list.cfg
 
 bash testing/simulator.sh ../../../scratch_nas/778/778-1500ng/778-1500ng_albacore-2.1.3/ testing/simulator_out 5
