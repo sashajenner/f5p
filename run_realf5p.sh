63e7b164e7d66be03dfadc53d1b9944f005e1378
Sasha Jenner <jenner.sasha@gmail.com>
2019-12-16 17:06:46 +1100

    can be run in one go

--- run_realf5p.sh
+++ run_realf5p.sh
@@ -1,4 +1,3 @@
-#bash testing/simulator.sh ../../../scratch_nas/778/778-1500ng/778-1500ng_albacore-2.1.3/ testing/simulator_out 5 # do this before next command
+bash testing/simulator.sh ../../../scratch_nas/778/778-1500ng/778-1500ng_albacore-2.1.3/ testing/simulator_out 5 & # execute simulator in the background
 
-bash monitor/monitor.sh testing/simulator_out/fast5/ testing/simulator_out/fastq/ |
-/usr/bin/time -v ./f5pl_realtime data/ip_list.cfg
+bash monitor/monitor.sh testing/simulator_out/fast5/ | /usr/bin/time -v ./f5pl_realtime data/ip_list.cfg # monitor the new file creation in fast5 folder and execute realtime f5 pipeline
