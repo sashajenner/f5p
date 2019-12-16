38e73a1b0950e15ab2b71a705510204f4f574a16
Sasha Jenner <jenner.sasha@gmail.com>
2019-12-16 17:07:35 +1100

    colour green enabled with output

--- testing/simulator.sh
+++ testing/simulator.sh
@@ -24,6 +24,8 @@ FQ_DIR="$INPUT_DIR"/fastq/
 
 TIME=$3
 
+GREEN="\033[34m"
+NORMAL="\033[0;39m"
 i=0 # initialise a file counter
 
 ## Iterate through the files
@@ -35,14 +37,21 @@ for filename_path in $F5_DIR/*.tar; do # files with tar extension in the fast5 d
 	filename="${filename_pathless%%.*}" # extract the filename without the extension nor the path
 
 	## Copy the corresponding fast5 and fastq to the output directory
-
+    
+    printf $GREEN # set font colour to green
 	echo -n "copying $i" # print without newline
+    printf $NORMAL # set font colour back to normal
+
 	# if file copying fails
 	if [ "$(mkdir -p $OUTPUT_DIR/fast5 && cp $F5_DIR/$filename.fast5.tar "$_")" -a \
 		"$(mkdir -p $OUTPUT_DIR/fastq && cp $FQ_DIR*$filename.fastq.gz "$_")" == 0 ]; then
-		echo " -> failed"
-	else 
-		echo " -> finished"
+        printf $GREEN
+		echo " -> failed copy $i"
+        printf $NORMAL
+	else
+        printf $GREEN
+		echo " -> finished copy $i"
+        printf $NORMAL
 	fi
 
 	sleep $TIME # pause for a given time
