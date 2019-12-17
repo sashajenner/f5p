df2c1a1c7e21f8b385f158a813646f5ec7c1a394
Sasha Jenner <jenner.sasha@gmail.com>
2019-12-17 11:08:29 +1100

    changes

--- run_realf5p.sh
+++ run_realf5p.sh
@@ -1,3 +1,5 @@
-bash testing/simulator.sh ../../../scratch_nas/778/778-1500ng/778-1500ng_albacore-2.1.3/ testing/simulator_out 5 & # execute simulator in the background
+NO_FILES=1
 
-bash monitor/monitor.sh testing/simulator_out/fast5/ | /usr/bin/time -v ./f5pl_realtime data/ip_list.cfg # monitor the new file creation in fast5 folder and execute realtime f5 pipeline
+bash monitor/monitor.sh -n $NO_FILES testing/simulator_out/fast5/ | /usr/bin/time -v ./f5pl_realtime data/ip_list.cfg & # monitor the new file creation in fast5 folder and execute realtime f5 pipeline
+
+bash testing/simulator.sh ../../../scratch_nas/778/778-1500ng/778-1500ng_albacore-2.1.3/ testing/simulator_out 5 $NO_FILES # execute simulator in the background
