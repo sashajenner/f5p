df21c2e01bd7b1391039c65ca20b4ae33b7af837
Sasha Jenner <jenner.sasha@gmail.com>
2019-12-18 14:03:37 +1100

    updated run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-
 # @author: Hasindu Gamaarachchi (hasindu@unsw.edu.au)
 
 ###############################################################################
 
-#some changeable definitions
+## Some changeable definitions
 
-#Folder containing the dataset
+# folder containing the dataset
 FOLDER=/mnt/f5p_example_dataset/
 
-#Folder containing the fast5 tar files
+# folder containing the fast5 tar files
 FAST5FOLDER=$FOLDER/fast5
 
-#The script to be copied and run on worker nodes
+# the script to be copied and run on worker nodes
 PIPELINE_SCRIPT="scripts/fast5_pipeline.sh"
 
 ###############################################################################
 
-#a test before cleaning logs
+# test before cleaning logs
 while true; do
     read -p "This will overwrite stats from the previous run. Do you wish to continue? (y/n) " yn
     case $yn in
@@ -27,35 +26,37 @@ while true; do
     esac
 done
 
-#a freshly compiled f5pl
+exit # testing
+
+# freshly compile f5pl
 make clean && make || exit 1
 
-#clean temporary locations on NAS and the worker nodes
+# clean temporary locations on NAS and the worker nodes
 rm -rf /scratch_nas/scratch/*
 cleanscratch.sh #https://github.com/hasindu2008/nanopore-cluster/blob/master/system/cleanscratch.sh
 
-#remove previous logs
+# remove previous logs
 test -d data/logs && rm -r data/logs
 mkdir data/logs || exit 1
 
-#create folders to copy the results (SAM files, BAM files, logs and methylation calls)
+# create folders to copy the results (SAM files, BAM files, logs and methylation calls)
 test -d $FOLDER/sam || mkdir $FOLDER/sam || exit 1
 test -d $FOLDER/bam || mkdir $FOLDER/bam || exit 1
 test -d $FOLDER/log2 || mkdir $FOLDER/log2 || exit 1
 test -d $FOLDER/methylation || mkdir $FOLDER/methylation || exit 1
 
-#check  the existence of the folder containing tar files
+# check  the existence of the folder containing tar files
 test -d $FAST5FOLDER || exit 1
-#make a list of tar files
+# make a list of tar files
 ls $FAST5FOLDER/*.tar -lhS | awk '{print $9}' > data/dev.cfg || exit 1;
 
-#copy the pipeline script accrross the worker nodes
+# copy the pipeline script accrross the worker nodes
 ansible all -m copy -a "src=$PIPELINE_SCRIPT dest=/nanopore/bin/fast5_pipeline.sh mode=0755" 
 
-#run f5pl
+# run f5pl
 /usr/bin/time -v ./f5pl data/ip_list.cfg data/dev.cfg 2>&1 | tee log.txt
 
-#handle the logs
+# handle the logs
 ansible all -m shell -a "cd /nanopore/scratch && tar zcvf logs.tgz *.log"
 gather.sh /nanopore/scratch/logs.tgz data/logs/log tgz #https://github.com/hasindu2008/nanopore-cluster/blob/master/system/gather.sh
 
