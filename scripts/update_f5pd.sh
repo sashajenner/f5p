3d333ae2953da6e2d646b662816af2f9e6ab8a94
Sasha Jenner <jenner.sasha@gmail.com>
2019-12-20 22:21:47 +1100

    small edit

--- scripts/update_f5pd.sh
+++ scripts/update_f5pd.sh
@@ -1,5 +1,5 @@
-ssh rock1 "cd ~/f5p && make clean && make" || exit 1
-scp rock1:~/f5p/f5pd ./f5pd || exit 1
+ssh rock64@rock1 "cd ~/f5p && make clean && make" || exit 1
+scp rock64@rock1:~/f5p/f5pd ./f5pd || exit 1
 ansible all -m shell -a "service f5pd stop" -K -b
 ansible all -m copy -a "src=./f5pd dest=/nanopore/bin/f5pd mode=0755"
 ansible all -m shell -a "service f5pd start" -K -b
