c3cebbdf884584816e2cb0c5d16ecd9781c21fe9
Sasha Jenner <jenner.sasha@gmail.com>
2019-12-20 22:20:20 +1100

    now with timeout option

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -5,7 +5,10 @@
 USAGE="Usage: $0 [options ...] <monitor_dirs>"
 : ${1?$USAGE} # require 1 parameter else give usage msg
 
+TEMP_FILE=temp # temporary file
+
 monitor_dirs=() # declare empty list of directories to monitor
+timeout=false # no timeout enabled by default
 
 ## Handle flags
 while [ ! $# -eq 0 ]; do # while there are arguments
@@ -18,10 +21,46 @@ while [ ! $# -eq 0 ]; do # while there are arguments
 
         --help | -h)
             echo $USAGE
-            echo "Flags:
+            echo 'Flags:
 -h, --help          help message
--n, --num_files     exits after given number of files"
+-n, --num_files     exits after given number of files
+-t, --timeout       exits after a specified time period of no new files
+        -s, --seconds   timeout format in seconds
+        -m, --minutes   "---------------" minutes
+        -hr, --hours    "---------------" hours'
             exit
+            ;;
+
+        --timeout | -t)
+            TIME_INACTIVE=$3 # time to wait until timeout
+            timeout=true # set timeout option on
+
+            case "$2" in
+                --seconds | -s)
+                    TIME_FACTOR=1 # 1 sec in a sec  
+                    shift
+                    ;;
+                --minutes | -m)
+                    TIME_FACTOR=60 # 60 sec in a min
+                    shift
+                    ;;
+                --hours | -hr)
+                    TIME_FORMAT=3600 # 3600 sec in an hour
+                    shift
+                    ;;
+                *)
+                    echo $USAGE
+                    echo 'Flags:
+-h, --help          help message
+-n, --num_files     exits after given number of files
+-t, --timeout       exits after a specified time period of no new files
+        -s, --seconds   timeout format in seconds
+        -m, --minutes   "---------------" minutes
+        -hr, --hours    "---------------" hours'
+                    exit
+                    ;;
+            esac
+            shift
             ;;
 
         *) 
@@ -36,15 +75,64 @@ while [ ! $# -eq 0 ]; do # while there are arguments
     shift
 done
 
+reset_timer() {
+    echo 0 > $TEMP_FILE # send flag to reset timer
+}
+
+exit_safely() {
+    rm $TEMP_FILE # remove the temporary file
+    kill -PIPE 0 # kill current and child processes
+}
+
+touch $TEMP_FILE # create the temporary file
+
+trap exit_safely EXIT # catch exit of script with function
 
 i=0 # define file counter
-
 ## Set up monitoring of all input directory indefinitely for a file being written or moved to them
-while read path action file; do
-    echo "$path$file" # output the absolute file path in such a case
-    
-    ((i++)) # increment file counter
-    if [ $NO_FILES -eq $i ]; then # exit after specified number of files found
-        break
-    fi 
-done < <(inotifywait -m ${monitor_dirs[@]} -e close_write -e moved_to) # pass output to while loop
+(
+    while read path action file; do
+
+        if $timeout; then # if timeout option set
+            reset_timer # reset the timer
+        fi
+        echo "$path$file" # output the absolute file path in such a case
+        
+        ((i++)) # increment file counter
+        if [ "$NO_FILES" = "$i" ]; then # exit after specified number of files found
+            echo -1 > $TEMP_FILE # send flag to main process
+            
+            while : # pause the script in while loop
+            do
+                sleep 1
+            done
+
+        fi
+
+    done < <(inotifywait -m ${monitor_dirs[@]} -e close_write -e moved_to) # pass output to while loop
+) & # push to the background
+
+if $timeout; then # if timeout option set
+    reset_timer # reset the timer
+fi
+while $timeout; do
+
+    # if 0 flag in temporary file
+    if [ "$(cat $TEMP_FILE)" = "0" ]; then # reset the timer
+        SECONDS=0
+        echo > $TEMP_FILE # empty contents of temp file
+    fi
+
+    # if there has been no files created in a specified period of time exit program
+    if [ $((SECONDS/TIME_FACTOR)) = "$TIME_INACTIVE" -o "$(cat $TEMP_FILE)" = "-1" ]; then
+        exit
+    fi
+
+done
+
+while : ; do # while true
+    # if -1 flag in temporary file
+    if [ "$(cat $TEMP_FILE)" = "-1" ]; then
+        exit
+    fi
+done
