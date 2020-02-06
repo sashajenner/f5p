bda45721edf217bc0fbd49ce3d2efe1b67dfd294
Sasha Jenner <jenner.sasha@gmail.com>
2020-02-07 09:34:50 +1100

    new test

--- test.sh
+++ test.sh
@@ -12,15 +12,15 @@ cp /dev/null screenlog.0 # Empty previous log file
 #     echo y | 
 #     bash run.sh -f --NA -m /mnt/simulator_out -8 /mnt/NA12878_cq --real -t -a"
 
-# Run 778-1500 realtime simulation in the background  :::Failure | weird failure at mkdir | dev*.cfg didn't copy:::
-screen -S sim_778-1500 -L -d -m bash -c "
-   echo y | 
-   bash run.sh -f --778 -m /mnt/simulator_out -8 /mnt/778/778-1500ng/778-1500ng_albacore-2.1.3/ --real -t -a"
+# Run 778-1500 realtime simulation in the background  :::Success:::
+# screen -S sim_778-1500 -L -d -m bash -c "
+#    echo y | 
+#    bash run.sh -f --778 -m /mnt/simulator_out -8 /mnt/778/778-1500ng/778-1500ng_albacore-2.1.3/ --real -t -a"
 
 # Run 778-5000 realtime simulation in the background  :::Failure | simulator.sh had error:::
-# screen -S sim_778-5000 -L -d -m bash -c "
-#    echo y | 
-#    bash run.sh -f --778 -m /mnt/simulator_out -8 /mnt/778/778-5000ng/778-5000ng_albacore-2.1.3/ --real -t -a"
+screen -S sim_778-5000 -L -d -m bash -c "
+   echo y | 
+   bash run.sh -f --778 -m /mnt/simulator_out -8 /mnt/778/778-5000ng/778-5000ng_albacore-2.1.3/ --real -t -a"
 
 # Run zebra realtime simulation in the background  :::Success | but file didn't get analysed properly:::
 # screen -S sim_zebra-tiny -L -d -m bash -c "
