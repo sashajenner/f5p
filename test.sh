add430280306ba24c89efa97035b5bc05412475a
Sasha Jenner <jenner.sasha@gmail.com>
2020-02-14 17:04:39 +1100

    further testing

--- test.sh
+++ test.sh
@@ -7,10 +7,10 @@ cp /dev/null screenlog.0 # Empty previous log file
 #    echo y | 
 #    bash run.sh -f --NA -m /mnt/simulator_out -8 /mnt/NA12878_cq --n=10 -t -m 10" # 10 batches, 5 min timeout
 
-# Run NA realtime simulation in the background  :::Failure |1 simulator.sh had error |2 run.sh irect command not found:::
-screen -S sim_NA -L -d -m bash -c "
-    echo y | 
-    bash run.sh -f --NA -m /mnt/simulator_out -8 /mnt/NA12878_cq --real -t -a"
+# Run NA realtime simulation in the background  :::Failure | some analysis failures:::
+# screen -S sim_NA -L -d -m bash -c "
+#     echo y | 
+#     bash run.sh -f --NA -m /mnt/simulator_out -8 /mnt/NA12878_cq --real -t -a"
 
 # Run 778-1500 realtime simulation in the background  :::Success:::
 # screen -S sim_778-1500 -L -d -m bash -c "
@@ -22,17 +22,17 @@ screen -S sim_NA -L -d -m bash -c "
 #    echo y | 
 #    bash run.sh -f --778 -m /mnt/simulator_out -8 /mnt/778/778-5000ng/778-5000ng_albacore-2.1.3/ --real -t -a"
 
-# Run zebra realtime simulation in the background  :::Success | but file didn't get analysed properly:::
+# Run zebra realtime simulation in the background  :::Failure | but file didn't get analysed properly:::
 # screen -S sim_zebra-tiny -L -d -m bash -c "
 #    echo y | 
 #    bash run.sh -f --zebra -m /mnt/simulator_out -8 /mnt/zebrafish/zebrafish_tiny/ --real -t -a"
 
 # Run zebra realtime simulation in the background  :::Failure | didn't end | didn't get detected:::
-# screen -S sim_zebra-actual -L -d -m bash -c "
-#    echo y | 
-#    bash run.sh -f --zebra -m /mnt/simulator_out -8 /mnt/zebrafish/zebrafish_test/ --real -t -a"
+screen -S sim_zebra-actual -L -d -m bash -c "
+   echo y | 
+   bash run.sh -f --zebra -m /mnt/simulator_out -8 /mnt/zebrafish/zebrafish_test/ --real -t -a"
 
-# Run zebra normal simulation in the background :::Success | but file didn't get analysed properly:::
+# Run zebra normal simulation in the background :::Failure | but file didn't get analysed properly:::
 # screen -S normal_zebra -L -d -m bash -c "
 #    echo y | 
 #    bash run.sh -f --zebra -m /mnt/simulator_out -8 /mnt/zebrafish/zebrafish_test/ --n=1 -t -s 20" # 1 batche, 20 sec timeout
