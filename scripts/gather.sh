e6d2b0567ed88a860d4ba97ca9f9de8f01c62390
Sasha Jenner <jenner.sasha@gmail.com>
2020-02-17 10:09:43 +1100

    generic gather method

--- scripts/gather.sh
+++ scripts/gather.sh
@@ -1,13 +1,15 @@
 if [ $# -ne 4 ]; then
-	echo "usage $0 ip_prefix file_prefix sourcefile dest_path_and_prefix extension"
+	echo "usage $0 ip_list sourcefile dest_path_and_prefix extension"
 	exit 1
 fi
 
-ip_prefix=$1
+ip_list=$1
 source=$2
 file_prefix=$3
 suffix=$4
 
-for i in $(seq 1 16); do
-	scp $ip_prefix.$i:$source $file_prefix$i.$suffix; 
+i=0
+for ip in $(cat $ip_list); do
+	(( i ++ ));
+	scp $ip:$source $file_prefix$i.$suffix;
 done
