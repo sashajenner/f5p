ad5d3d2048c82ea5fdedaa280bd7dce0ac9a12d9
Sasha Jenner <jenner.sasha@gmail.com>
2020-02-28 15:15:39 +1100

    webf5pd comments and improvements for alteration

--- scripts/install_webf5pd_service.sh
+++ scripts/install_webf5pd_service.sh
@@ -1,3 +1,4 @@
+cp webf5pd /nanopore/bin/ # (3): Change '/nanopore/bin' to point to the location of (2) in webf5pd.service
 cp scripts/webf5pd.service /etc/systemd/system/webf5pd.service
 systemctl daemon-reload
 systemctl enable f5pd
